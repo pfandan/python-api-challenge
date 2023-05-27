af40b77d9824c5aee3bfee37b63c0601f782b231
Poonam <pfandan@gmail.com>
2023-05-26 18:59:30 -0700

    updtes

--- api_keys.py
+++ api_keys.py
@@ -1,1 +1,4 @@
-openweather_api_key = 'e229c911d87f85485a8bf9516fe112cf'
+#weather_api_key = 'e229c911d87f85485a8bf9516fe112cf'
+weather_api_key= 'e0122c3c4c700b5389eb6bff148eb595'
+geoapify_key = '2f8a8ddc15734faea1969553ef24245c'
+
